f645e76c5d9c05058ad123830e23ffe66007a109
Nihit Agarwal <nihitagarwal2006@gmail.com>
2025-12-30 18:36:01 +0530

    Fix CORS and finalize backend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],   # THIS allows OPTIONS + POST
+    allow_headers=["*"],
+)
 
 class Message(BaseModel):
     text: str
